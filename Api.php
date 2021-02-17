bc789e7d8c7a398e49aabcec772015a38d3eb765
evertdf <edefeij@live.nl>
2021-02-17 19:06:04 +0100

    iproved api basex

--- Api.php
+++ Api.php
@@ -2,6 +2,8 @@
 namespace Modules\BaseX {
     trait Api {
         use \Modules\Node;
+        use \Modules\BaseX;
+        
         public function xpath(string $query) : \DOMNodeList {            
             $request = new $this->request;
             $xpath = new \DOMXPath($request->fetchDOM($query));
